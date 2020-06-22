f4fde6cc74657d667402f436d2b8658e9d02c250
Nicholas Frechette <zeno490@gmail.com>
2020-06-21 23:05:24 -0400

    wip

--- includes/rtm/quatf.h
+++ includes/rtm/quatf.h
@@ -430,10 +430,17 @@ namespace rtm
 #if defined(RTM_SSE2_INTRINSICS)
 		constexpr __m128 signs = { -0.0F, -0.0F, -0.0F, 0.0F };
 		return _mm_xor_ps(input, signs);
-#elif defined(RTM_NEON_INTRINSICS)
+#elif defined(RTM_NEON_INTRINSICS) && 0
+		alignas(16) constexpr float signs_f[4] = { -1.0f, -1.0f, -1.0f, 1.0f };
+		const float32x4_t signs = *reinterpret_cast<const float32x4_t*>(&signs_f[0]);
+		return vmulq_f32(input, signs);
+#elif defined(RTM_NEON_INTRINSICS) && 0
 		alignas(16) constexpr uint32_t signs_u[4] = { 0x80000000U, 0x80000000U, 0x80000000U, 0 };
 		const uint32x4_t signs = *reinterpret_cast<const uint32x4_t*>(&signs_u[0]);
 		return vreinterpretq_f32_u32(veorq_u32(vreinterpretq_u32_f32(input), signs));
+#elif defined(RTM_NEON_INTRINSICS) && 0
+		const float32x4_t neg_input = vnegq_f32(input);
+		return vsetq_lane_f32(vgetq_lane_f32(input, 3), neg_input, 3);
 #else
 		return quat_set(-quat_get_x(input), -quat_get_y(input), -quat_get_z(input), quat_get_w(input));
 #endif
@@ -497,7 +504,66 @@ namespace rtm
 		const __m128 nlyrz_lxrz_lwrz_wlzrz = _mm_xor_ps(lyrz_lxrz_lwrz_lzrz, control_yxwz);
 		const __m128 result1 = _mm_add_ps(lzry_lwry_nlxry_nlyry, nlyrz_lxrz_lwrz_wlzrz);
 		return _mm_add_ps(result0, result1);
-#elif defined(RTM_NEON_INTRINSICS)
+#elif defined(RTM_NEON_INTRINSICS) && 0
+		alignas(16) constexpr uint32x4_t control_wzyx_f[4] = { 0, 0x80000000U, 0, 0x80000000U };
+		alignas(16) constexpr uint32x4_t control_zwxy_f[4] = { 0, 0, 0x80000000U, 0x80000000U };
+		alignas(16) constexpr uint32x4_t control_yxwz_f[4] = { 0x80000000U, 0, 0, 0x80000000U };
+
+		const uint32x4_t control_wzyx = *reinterpret_cast<const uint32x4_t*>(&control_wzyx_f[0]);
+		const uint32x4_t control_zwxy = *reinterpret_cast<const uint32x4_t*>(&control_zwxy_f[0]);
+		const uint32x4_t control_yxwz = *reinterpret_cast<const uint32x4_t*>(&control_yxwz_f[0]);
+
+		float32x2_t r_xy = vget_low_f32(rhs);
+		float32x2_t r_zw = vget_high_f32(rhs);
+
+		float32x4_t lxrw_lyrw_lzrw_lwrw = vmulq_lane_f32(lhs, r_zw, 1);
+
+		float32x4_t l_yxwz = vrev64q_f32(lhs);
+		float32x4_t l_wzyx = vcombine_f32(vget_high_f32(l_yxwz), vget_low_f32(l_yxwz));
+		float32x4_t lwrx_lzrx_lyrx_lxrx = vmulq_lane_f32(l_wzyx, r_xy, 0);
+
+		float32x4_t result0 = vaddq_f32(vreinterpretq_f32_u32(veorq_u32(vreinterpretq_u32_f32(lwrx_lzrx_lyrx_lxrx), control_wzyx)), lxrw_lyrw_lzrw_lwrw);
+
+		float32x4_t l_zwxy = vrev64q_f32(l_wzyx);
+		float32x4_t lzry_lwry_lxry_lyry = vmulq_lane_f32(l_zwxy, r_xy, 1);
+
+		float32x4_t result1 = vaddq_f32(vreinterpretq_f32_u32(veorq_u32(vreinterpretq_u32_f32(lzry_lwry_lxry_lyry), control_zwxy)), result0);
+
+		float32x4_t lyrz_lxrz_lwrz_lzrz = vmulq_lane_f32(l_yxwz, r_zw, 0);
+
+		return vaddq_f32(vreinterpretq_f32_u32(veorq_u32(vreinterpretq_u32_f32(lyrz_lxrz_lwrz_lzrz), control_yxwz)), result1);
+#elif defined(RTM_NEON_INTRINSICS) && 0
+		alignas(16) constexpr float control_wzyx_f[4] = { 1.0f, -1.0f, 1.0f, -1.0f };
+		alignas(16) constexpr float control_zwxy_f[4] = { 1.0f, 1.0f, -1.0f, -1.0f };
+		alignas(16) constexpr float control_yxwz_f[4] = { -1.0f, 1.0f, 1.0f, -1.0f };
+
+		const float32x4_t control_wzyx = *reinterpret_cast<const float32x4_t*>(&control_wzyx_f[0]);
+		const float32x4_t control_zwxy = *reinterpret_cast<const float32x4_t*>(&control_zwxy_f[0]);
+		const float32x4_t control_yxwz = *reinterpret_cast<const float32x4_t*>(&control_yxwz_f[0]);
+
+		const float32x2_t r_xy = vget_low_f32(rhs);
+		const float32x2_t r_zw = vget_high_f32(rhs);
+
+		const float32x4_t lxrw_lyrw_lzrw_lwrw = vmulq_lane_f32(lhs, r_zw, 1);
+
+		const float32x4_t l_yxwz = vrev64q_f32(lhs);
+		const float32x4_t l_wzyx = vcombine_f32(vget_high_f32(l_yxwz), vget_low_f32(l_yxwz));
+		const float32x4_t l_zwxy = vrev64q_f32(l_wzyx);
+
+		const float32x4_t lwrx_lzrx_lyrx_lxrx = vmulq_lane_f32(l_wzyx, r_xy, 0);
+		const float32x4_t lzry_lwry_lxry_lyry = vmulq_lane_f32(l_zwxy, r_xy, 1);
+		const float32x4_t lyrz_lxrz_lwrz_lzrz = vmulq_lane_f32(l_yxwz, r_zw, 0);
+
+#if defined(RTM_NEON64_INTRINSICS)
+		const float32x4_t result0 = vfmaq_f32(lxrw_lyrw_lzrw_lwrw, lwrx_lzrx_lyrx_lxrx, control_wzyx);
+		const float32x4_t result1 = vfmaq_f32(result0, lzry_lwry_lxry_lyry, control_zwxy);
+		return vfmaq_f32(result1, lyrz_lxrz_lwrz_lzrz, control_yxwz);
+#else
+		const float32x4_t result0 = vmlaq_f32(lxrw_lyrw_lzrw_lwrw, lwrx_lzrx_lyrx_lxrx, control_wzyx);
+		const float32x4_t result1 = vmlaq_f32(result0, lzry_lwry_lxry_lyry, control_zwxy);
+		return vmlaq_f32(result1, lyrz_lxrz_lwrz_lzrz, control_yxwz);
+#endif
+#elif defined(RTM_NEON_INTRINSICS) && 0
 		// Use shuffles and negation instead of loading constants and doing mul/xor.
 		// On ARM64, neg and shuffles are usually 2 cycles while xor is still 3 cycles.
 		// We have to shuffle things anyway, might as well leverage everything we can.
@@ -620,7 +686,74 @@ namespace rtm
 			__m128 result1 = _mm_add_ps(lzry_lwry_lxry_lyry, lyrz_lxrz_lwrz_lzrz);
 			return _mm_add_ps(result0, result1);
 		}
-#elif defined(RTM_NEON_INTRINSICS)
+#elif defined(RTM_NEON_INTRINSICS) && 0
+		alignas(16) constexpr float control_wzyx_f[4] = { 1.0f, -1.0f, 1.0f, -1.0f };
+		alignas(16) constexpr float control_zwxy_f[4] = { 1.0f, 1.0f, -1.0f, -1.0f };
+		alignas(16) constexpr float control_yxwz_f[4] = { -1.0f, 1.0f, 1.0f, -1.0f };
+
+		const float32x4_t control_wzyx = *reinterpret_cast<const float32x4_t*>(&control_wzyx_f[0]);
+		const float32x4_t control_zwxy = *reinterpret_cast<const float32x4_t*>(&control_zwxy_f[0]);
+		const float32x4_t control_yxwz = *reinterpret_cast<const float32x4_t*>(&control_yxwz_f[0]);
+
+		const float32x4_t inv_rotation = quat_conjugate(rotation);
+
+		// Normally when we multiply our inverse rotation quaternion with the input vector as a quaternion with W = 0.0.
+		// As a result, we can strip the whole part that uses W saving a few instructions.
+		// We also don't care about the result W lane but it comes for free.
+		// With ARMv7, it seems that floating point instructions aren't the bottleneck and using shuffles to move things
+		// around to take advantage of sign flipping like the other implementations does not benefit.
+
+		// temp = quat_mul(inv_rotation, vector_quat)
+		float32x4_t temp;
+		{
+			const float32x2_t r_xy = vget_low_f32(vector);
+			const float32x2_t r_zw = vget_high_f32(vector);
+
+			const float32x4_t l_yxwz = vrev64q_f32(inv_rotation);
+			const float32x4_t l_wzyx = vcombine_f32(vget_high_f32(l_yxwz), vget_low_f32(l_yxwz));
+			const float32x4_t l_zwxy = vrev64q_f32(l_wzyx);
+
+			const float32x4_t lwrx_lzrx_lyrx_lxrx = vmulq_lane_f32(l_wzyx, r_xy, 0);
+			const float32x4_t lzry_lwry_lxry_lyry = vmulq_lane_f32(l_zwxy, r_xy, 1);
+			const float32x4_t lyrz_lxrz_lwrz_lzrz = vmulq_lane_f32(l_yxwz, r_zw, 0);
+
+			const float32x4_t result0 = vmulq_f32(lwrx_lzrx_lyrx_lxrx, control_wzyx);
+
+#if defined(RTM_NEON64_INTRINSICS)
+			const float32x4_t result1 = vfmaq_f32(result0, lzry_lwry_lxry_lyry, control_zwxy);
+			temp = vfmaq_f32(result1, lyrz_lxrz_lwrz_lzrz, control_yxwz);
+#else
+			const float32x4_t result1 = vmlaq_f32(result0, lzry_lwry_lxry_lyry, control_zwxy);
+			temp = vmlaq_f32(result1, lyrz_lxrz_lwrz_lzrz, control_yxwz);
+#endif
+		}
+
+		// result = quat_mul(temp, rotation)
+		{
+			const float32x2_t r_xy = vget_low_f32(rotation);
+			const float32x2_t r_zw = vget_high_f32(rotation);
+
+			const float32x4_t lxrw_lyrw_lzrw_lwrw = vmulq_lane_f32(temp, r_zw, 1);
+
+			const float32x4_t l_yxwz = vrev64q_f32(temp);
+			const float32x4_t l_wzyx = vcombine_f32(vget_high_f32(l_yxwz), vget_low_f32(l_yxwz));
+			const float32x4_t l_zwxy = vrev64q_f32(l_wzyx);
+
+			const float32x4_t lwrx_lzrx_lyrx_lxrx = vmulq_lane_f32(l_wzyx, r_xy, 0);
+			const float32x4_t lzry_lwry_lxry_lyry = vmulq_lane_f32(l_zwxy, r_xy, 1);
+			const float32x4_t lyrz_lxrz_lwrz_lzrz = vmulq_lane_f32(l_yxwz, r_zw, 0);
+
+#if defined(RTM_NEON64_INTRINSICS)
+			const float32x4_t result0 = vfmaq_f32(lxrw_lyrw_lzrw_lwrw, lwrx_lzrx_lyrx_lxrx, control_wzyx);
+			const float32x4_t result1 = vfmaq_f32(result0, lzry_lwry_lxry_lyry, control_zwxy);
+			return vfmaq_f32(result1, lyrz_lxrz_lwrz_lzrz, control_yxwz);
+#else
+			const float32x4_t result0 = vmlaq_f32(lxrw_lyrw_lzrw_lwrw, lwrx_lzrx_lyrx_lxrx, control_wzyx);
+			const float32x4_t result1 = vmlaq_f32(result0, lzry_lwry_lxry_lyry, control_zwxy);
+			return vmlaq_f32(result1, lyrz_lxrz_lwrz_lzrz, control_yxwz);
+#endif
+		}
+#elif defined(RTM_NEON_INTRINSICS) && 0
 
 		// Normally when we multiply our inverse rotation quaternion with the input vector as a quaternion with W = 0.0.
 		// As a result, we can strip the whole part that uses W saving a few instructions.
@@ -709,6 +842,48 @@ namespace rtm
 			const float32x4_t result1 = vmlaq_lane_f32(result0, l_zwxy, r_xy, 1);
 			return vmlaq_lane_f32(result1, l_yxwz, r_zw, 0);
 		#endif
+		}
+#elif defined(RTM_NEON_INTRINSICS) && 0
+		const float32x4_t n_rotation = vnegq_f32(rotation);
+
+		// temp = quat_mul(inv_rotation, vector_quat)
+		float temp_x;
+		float temp_y;
+		float temp_z;
+		float temp_w;
+		{
+			const float lhs_x = quat_get_x(n_rotation);
+			const float lhs_y = quat_get_y(n_rotation);
+			const float lhs_z = quat_get_z(n_rotation);
+			const float lhs_w = quat_get_w(rotation);
+
+			const float rhs_x = quat_get_x(vector);
+			const float rhs_y = quat_get_y(vector);
+			const float rhs_z = quat_get_z(vector);
+
+			temp_x = (rhs_x * lhs_w) + (rhs_y * lhs_z) - (rhs_z * lhs_y);
+			temp_y = -(rhs_x * lhs_z) + (rhs_y * lhs_w) + (rhs_z * lhs_x);
+			temp_z = (rhs_x * lhs_y) - (rhs_y * lhs_x) + (rhs_z * lhs_w);
+			temp_w = -(rhs_x * lhs_x) - (rhs_y * lhs_y) - (rhs_z * lhs_z);
+		}
+
+		// result = quat_mul(temp, rotation)
+		{
+			const float lhs_x = temp_x;
+			const float lhs_y = temp_y;
+			const float lhs_z = temp_z;
+			const float lhs_w = temp_w;
+
+			const float rhs_x = quat_get_x(rotation);
+			const float rhs_y = quat_get_y(rotation);
+			const float rhs_z = quat_get_z(rotation);
+			const float rhs_w = quat_get_w(rotation);
+
+			const float x = (rhs_w * lhs_x) + (rhs_x * lhs_w) + (rhs_y * lhs_z) - (rhs_z * lhs_y);
+			const float y = (rhs_w * lhs_y) - (rhs_x * lhs_z) + (rhs_y * lhs_w) + (rhs_z * lhs_x);
+			const float z = (rhs_w * lhs_z) + (rhs_x * lhs_y) - (rhs_y * lhs_x) + (rhs_z * lhs_w);
+
+			return vector_set(x, y, z, z);
 		}
 #else
 		quatf vector_quat = quat_set_w(vector_to_quat(vector), 0.0f);
